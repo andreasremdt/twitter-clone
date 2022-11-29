af8888a3d68218b3c4f394df154741ecb0191e0e
Andreas Remdt <me@andreasremdt.com>
2022-11-29 09:21:19 +0100

    feat: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Twitter</title>
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
