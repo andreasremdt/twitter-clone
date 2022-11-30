a848694d51a5810f3b57013acf5e6b1c1dc10d5d
Andreas Remdt <me@andreasremdt.com>
2022-11-30 10:09:31 +0100

    feat: add refresh feed logic

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,32 +1,54 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import toast from "react-hot-toast";
 
 import TweetForm from "@/components/tweet-form";
 import TweetBox from "@/components/tweet-box";
+import fetchTweets from "@/lib/fetch-tweets";
 
 import type { Tweet } from "@/lib/types";
+import { useState } from "react";
 
 type Props = {
   tweets: Tweet[];
 };
 
-const Feed = ({ tweets }: Props) => (
-  <div className="col-span-7 lg:col-span-5 border-x pt-4 px-4">
-    <header className="flex items-center justify-between">
-      <h1 className="text-xl font-bold">Home</h1>
+const Feed = ({ tweets: tweetsProp }: Props) => {
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
 
-      <button type="button" className="transition-transform duration-500 ease-out hover:rotate-180 active:scale-125">
-        <ArrowPathIcon className="w-8 h-8 text-twitter" />
-      </button>
-    </header>
+  const handleRefresh = async () => {
+    const refreshToast = toast.loading("Refreshing...");
+    const tweets = await fetchTweets();
 
-    <TweetForm />
+    setTweets(tweets);
 
-    <div>
-      {tweets.map((tweet) => (
-        <TweetBox key={tweet._id} tweet={tweet} />
-      ))}
+    toast.success("Feed updated!", {
+      id: refreshToast,
+    });
+  };
+
+  return (
+    <div className="col-span-7 lg:col-span-5 border-x pt-4 px-4">
+      <header className="flex items-center justify-between">
+        <h1 className="text-xl font-bold">Home</h1>
+
+        <button
+          type="button"
+          className="transition-transform duration-500 ease-out hover:rotate-180 active:scale-125"
+          onClick={handleRefresh}
+        >
+          <ArrowPathIcon className="w-8 h-8 text-twitter" />
+        </button>
+      </header>
+
+      <TweetForm />
+
+      <div>
+        {tweets.map((tweet) => (
+          <TweetBox key={tweet._id} tweet={tweet} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Feed;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import { Toaster } from "react-hot-toast";
+
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Toaster />
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
